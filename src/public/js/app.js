1f2842294a59637d2469d8e06fccf83836667777
11hertz <reachzin@naver.com>
2023-11-19 21:46:35 +0900

    2.4 Rooms

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,15 +2,24 @@ const socket = io();
 
 const $welcome = document.getElementById('welcome');
 const $form = $welcome.querySelector('form');
+const $room = document.getElementById('room');
 
-function backendDone(msg) {
-  console.log(`backend says: `, msg);
+$room.hidden = true;
+
+let roomName;
+
+function showRoom() {
+  $welcome.hidden = true;
+  $room.hidden = false;
+  const $roomName = document.querySelector('h3');
+  $roomName.innerText = `Room ${roomName}`;
 }
 
 function handleRoomSubmit(event) {
   event.preventDefault();
   const $input = $form.querySelector('input');
-  socket.emit('enter_room', $input.value, backendDone);
+  socket.emit('enter_room', $input.value, showRoom);
+  roomName = $input.value;
   $input.value = '';
 }
 
