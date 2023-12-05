5221f8fcadee9490aa5ae1f47fdeb5efeeac3e1e
11hertz <reachzin@naver.com>
2023-12-05 21:16:42 +0900

    3.8 Senders

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -83,6 +83,13 @@ function handleMcameraBtn() {
 
 async function handleCameraChange() {
   await getMedia($camerasSelect.value);
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find(sender => sender.track.kind === 'video');
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
 $muteBtn.addEventListener('click', handleMuteBtn);
